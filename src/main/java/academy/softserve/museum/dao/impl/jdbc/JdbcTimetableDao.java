effd9d1f949c6a0113b30c43d7f75e537c878a97
Oleh Kopylchak <oleg12311999@gmail.com>
2019-10-14 16:20:36 +0300

    Delted implementation of findById in JdbcTimetableDao

--- src/main/java/academy/softserve/museum/dao/impl/jdbc/JdbcTimetableDao.java
+++ src/main/java/academy/softserve/museum/dao/impl/jdbc/JdbcTimetableDao.java
@@ -39,31 +39,7 @@ public class JdbcTimetableDao implements TimetableDao {
 
     @Override
     public Optional<Timetable> findById(long id) {
-        String FIND_STATISTIC = "" +
-                "SELECT tt.id AS timetable_id, e.id AS employee_id, ex.id AS excursion_id, " +
-                "e.first_name, e.last_name, e.position, e.login, e.password, " +
-                "tt.date_start, tt.date_end, " +
-                "DATE_PART('hour', date_end - date_start) * 60 + " +
-                "DATE_PART('minute', date_end - date_start) AS excursion_time, " +
-                "ex.name AS excursion_name " +
-                "FROM employees AS e " +
-                "INNER JOIN timetable AS tt " +
-                "ON e.id = tt.employee_id " +
-                "INNER JOIN excursion AS ex " +
-                "ON tt.excursion_id = ex.id;";
-
-        try (PreparedStatement statement = connection.prepareStatement(FIND_STATISTIC)) {
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    return Optional.of(new TimetableRowMapper().mapRow(resultSet));
-                }
-
-                return Optional.empty();
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
+        return null;
     }
 
     @Override
